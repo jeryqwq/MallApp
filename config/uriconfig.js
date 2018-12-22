f3d10916088c3c627ad1f26980200be369933719
jeryqwq <jery1997@foxmail.com>
2018-12-22 10:03:33 +0800

    Update uriconfig.js

--- config/uriconfig.js
+++ config/uriconfig.js
@@ -1,25 +1,13 @@
 //宿舍电脑局域网
-// networkHost="192.168.137.1"
-// imgServerPort="8080"
-// imgAgreement="http"
-// apiAgreement="http"
-// apiServerPort="3000"
-// apiAddressFront=apiAgreement+"://"+ networkHost+":"+apiServerPort+"/"
-// imgAddressFront=imgAgreement+"://" +networkHost+":"+imgServerPort+'/'
-// module.exports={apiAddressFront,imgAddressFront}
+networkHost="192.168.137.1"
+
 
 // 公司IP
 // networkHost="192.168.4.153";
-// imgServerPort="8080";
-// imgAgreement="http";
-// apiAgreement="http";
-// apiServerPort="3000";
-// apiAddressFront=apiAgreement+"://"+ networkHost+":"+apiServerPort+"/";
-// imgAddressFront=imgAgreement+"://" +networkHost+":"+imgServerPort+'/';
-// module.exports={apiAddressFront,imgAddressFront}
+
 
 //后台笔记本局域网IP
-networkHost="192.168.250.74";
+// networkHost="192.168.250.74";
 imgServerPort="8080";
 imgAgreement="http";
 apiAgreement="http";
